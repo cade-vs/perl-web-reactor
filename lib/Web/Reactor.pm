83858ea4b6edea2ec6e94afd17efa7dd835b56b6
Vladi Belperchinov-Shabanski <cade@datamax.bg>
2013-08-13 13:17:54 +0300

    maint

--- lib/Web/Reactor.pm
+++ lib/Web/Reactor.pm
@@ -1416,9 +1416,9 @@ forwarding.
 
 =head1 GITHUB REPOSITORY
 
-  https://github.com/cade4/perl-web-reactor
+  https://github.com/cade-vs/perl-web-reactor
   
-  git clone git://github.com/cade4/perl-web-reactor.git
+  git clone git://github.com/cade-vs/perl-web-reactor.git
 
 =head1 AUTHOR
 

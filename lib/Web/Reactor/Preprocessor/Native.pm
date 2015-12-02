f460f123b67388ead9d10c8f38ea63744ba457a9
Vladi Belperchinov-Shabanski <cade@datamax.bg>
2015-12-02 04:18:00 +0200

    log-messages

--- lib/Web/Reactor/Preprocessor/Native.pm
+++ lib/Web/Reactor/Preprocessor/Native.pm
@@ -103,7 +103,7 @@ sub load_file
     return undef;
     }
 
-  warn( "** LOAD HTML PAGE: filename is [$fn]" ) if $reo->is_debug();
+  $reo->log_debug( "debug: preprocessor load page filename [$fn]" );
   my $fdata = file_load( $fn );
   $self->{ 'FILE_CACHE' }{ $lang }{ $pn } = $fdata;
 

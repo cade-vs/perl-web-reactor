cd071fc5374268cc830476f5095b67f764f58888
Vladi Belperchinov-Shabanski <cade@noxrun.com>
2022-09-19 22:21:42 +0300

    maint

--- lib/Web/Reactor/HTML/Form.pm
+++ lib/Web/Reactor/HTML/Form.pm
@@ -567,9 +567,10 @@ sub input
     my $empty_key   = str_html_escape( $opt{ 'EMPTY_KEY' } );
     my $input_id    = $self->create_uniq_id();
     my $datalist_id = $self->create_uniq_id();
-    $text .= "\n\n\n\n\n<input id=$input_id type=hidden    name='$name' value='$key'          form='$form_id'      >";
-    $text .= "\n<input class='$class' value='$value' list=$datalist_id $options form='$form_id' $args data-input-id=$input_id data-empty-key='$empty_key' onchange='return reactor_datalist_change( this, $resub )'>$clear_tag";
-    $text .= "\n<datalist id=$datalist_id>";
+    $class .= " search_list";
+    $text  .= "\n\n\n\n\n<input id=$input_id type=hidden    name='$name' value='$key'          form='$form_id'      >";
+    $text  .= "\n<input class='$class' value='$value' list=$datalist_id $options form='$form_id' $args data-input-id=$input_id data-empty-key='$empty_key' onchange='return reactor_datalist_change( this, $resub )'>$clear_tag";
+    $text  .= "\n<datalist id=$datalist_id>";
     for my $e ( @$datalist )
       {
       my $k = $e->{ 'KEY'   };
